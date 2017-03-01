a2324b13758b3fb94d9a937c0834e613579c9629
xta13 <xta13@sfu.ca>
2017-03-01 13:01:23 +0800

    Main Page added

---
+++ src/app/+recipes/mainRecipe.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mainRecipe',
+  template: `
+    <div>
+      <button type="button" class="btn-class" >
+        <img class="img" src="./app/+recipes/grey.jpg" alt="grey box">
+        <span class="recipeName"> Cook Book Recipe #1 </span>
+      </button>
+    </div>
+  `
+})
+export class MainRecipeComponent { }
