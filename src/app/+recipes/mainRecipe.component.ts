026b2c37ad1f88dd0884617b8998030b4f404905
xta13 <xta13@sfu.ca>
2017-03-02 11:54:05 +0800

    main page done

--- src/app/+recipes/mainRecipe.component.ts
+++ src/app/+recipes/mainRecipe.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'mainRecipe',
   template: `
     <div>
       <button type="button" class="btn-class" >
-        <img class="img" src="./app/+recipes/grey.jpg" alt="grey box">
-        <span class="recipeName"> Cook Book Recipe #1 </span>
+        <img class="img" src="./app/+recipes/1.jpg" alt="recipe1_sample"><br>
+        <span class="recipeName"> {{recipe1|json}}</span>
+      </button>
+      <button type="button" class="btn-class" >
+        <img class="img" src="./app/+recipes/2.jpg" alt="recipe1_sample"><br>
+        <span class="recipeName"> {{recipe2|json}}</span>
+      </button>
+      <button type="button" class="btn-class" >
+        <img class="img" src="./app/+recipes/3.jpg" alt="recipe1_sample"><br>
+        <span class="recipeName"> {{recipe3|json}}</span>
       </button>
     </div>
-  `
+  `,
+  styleUrls: ['./recipes.component.css']
 })
-export class MainRecipeComponent { }
+export class MainRecipeComponent {
+    recipe1: Object;
+    recipe2: Object;
+    recipe3: Object;
+    constructor(private http:Http) {
+        this.http.get('./app/+recipes/recipes.json')
+                 .subscribe(res => {this.recipe1 = res.json().Recipes[0].Name;
+                                    this.recipe2 = res.json().Recipes[1].Name;
+                                    this.recipe3 = res.json().Recipes[2].Name;
+                  });
+
+    }}
