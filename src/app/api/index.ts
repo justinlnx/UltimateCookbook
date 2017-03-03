c71a48460927597cd33f74ac1cf1e7adbefb65cd
CarlCui <carlcui1993@outlook.com>
2017-03-03 00:10:47 -0800

    addressing comments: newline at eof and generateGuid

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,3 +1,4 @@
 export {FirebaseListObservable} from 'angularfire2';
 export {ApiService} from './api.service';
+export {generateGuid} from './guid';
 export {WebServiceException} from './WebServiceException';
