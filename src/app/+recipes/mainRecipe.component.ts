eb8014a483ef423789d20fb8fb2d832f5f4e498a
njliang <njliang@sfu.ca>
2017-03-04 17:30:47 -0800

    9 remove main recipe template

--- src/app/+recipes/mainRecipe.component.ts
+++ src/app/+recipes/mainRecipe.component.ts
@@ -4,55 +4,34 @@ import {ApiService, FirebaseListObservable} from '../api';
 
 @Component({
   selector: 'mainRecipe',
-  // template: `
-  //   <div>
-  //     <button type="button" class="btn-class" >
-  //       <img class="img" src="./app/+recipes/1.jpg" alt="recipe1_sample"><br>
-  //       <span class="recipeName"> {{recipe1|json}}</span>
-  //     </button>
-  //     <button type="button" class="btn-class" >
-  //       <img class="img" src="./app/+recipes/2.jpg" alt="recipe1_sample"><br>
-  //       <span class="recipeName"> {{recipe2|json}}</span>
-  //     </button>
-  //     <button type="button" class="btn-class" >
-  //       <img class="img" src="./app/+recipes/3.jpg" alt="recipe1_sample"><br>
-  //       <span class="recipeName"> {{recipe3|json}}</span>
-  //     </button>
-  //   </div>
-  // `,
   template: `
     <div>
-      <ul>
-        <li *ngFor="let recipe of recipeNames; let i = index">
-          <button type="button" class="btn-class">
-            <img class="img" src="./app/+recipes/{{i}}.jpg" alt="recipe1_sample"><br>
-            <span class="recipeName"> {{recipe|json}}</span>
-          </button>
-        </li>
-      </ul>
+      <button type="button" class="btn-class" >
+        <img class="img" src="./app/+recipes/1.jpg" alt="recipe1_sample"><br>
+        <span class="recipeName"> {{recipe1|json}}</span>
+      </button>
+      <button type="button" class="btn-class" >
+        <img class="img" src="./app/+recipes/2.jpg" alt="recipe1_sample"><br>
+        <span class="recipeName"> {{recipe2|json}}</span>
+      </button>
+      <button type="button" class="btn-class" >
+        <img class="img" src="./app/+recipes/3.jpg" alt="recipe1_sample"><br>
+        <span class="recipeName"> {{recipe3|json}}</span>
+      </button>
     </div>
   `,
   styleUrls: ['./recipes.component.css']
 })
 export class MainRecipeComponent {
-  // recipe1: Object;
-  // recipe2: Object;
-  // recipe3: Object;
-
-  // constructor(private apiService: ApiService) {
-  //   this.apiService.getAllRecipes().subscribe(res => {
-  //     this.recipe1 = res[0].name;
-  //     this.recipe2 = res[1].name;
-  //     this.recipe3 = res[2].name;
-  //   });
-  // }
-  recipeNames: Object[];
+  recipe1: Object;
+  recipe2: Object;
+  recipe3: Object;
 
   constructor(private apiService: ApiService) {
-    this.apiService.getAllRecipes().subscribe(response => {
-      response.forEach(recipe => {
-        this.recipeNames.push(recipe.name);
-      });
-    })
+    this.apiService.getAllRecipes().subscribe(res => {
+      this.recipe1 = res[0].name;
+      this.recipe2 = res[1].name;
+      this.recipe3 = res[2].name;
+    });
   }
 }
