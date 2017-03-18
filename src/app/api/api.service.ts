49e29f492141d657799fcdf476519f20351a777e
xta13 <xta13@sfu.ca>
2017-03-18 15:48:53 +0800

    put API

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
-import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
-import {Observable} from 'rxjs/Observable';
-import {ErrorReportService} from '../error-report';
-import {generateGuid} from './guid';
+import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import { ErrorReportService } from '../error-report';
+import { generateGuid } from './guid';
 
 type Guid = string;
 
@@ -39,7 +39,7 @@ export class ApiService {
 
   public addRecipe(recipe: PushRecipe): void {
     this.recipeListObservable.push(recipe).then(
-        (_) => console.log('success.'), (err) => this.errorReportService.send(err.message));
+      (_) => console.log('success.'), (err) => this.errorReportService.send(err.message));
   }
 
   public deleteRecipe($key: string): void {
@@ -52,7 +52,85 @@ export class ApiService {
     }
   }
 
-  public updateRecipe(recipe: Recipe): Observable<boolean> {
-    return Observable.of(true);
+  public updateRecipe($key: string, attributeName: string, newValue: any): void {
+
+    if ($key === undefined || $key === null || $key.length === 0) {
+      let exception = 'Invalid Key';
+      this.errorReportService.send(exception);
+    }
+    else {
+      if (attributeName === "steps") {
+        this.updateSteps($key, newValue);
+      }
+      else if (attributeName === "author") {
+        this.updateAuthor($key, newValue);
+      }
+      else if (attributeName === "avatar") {
+        this.updateAvatar($key, newValue);
+      }
+      else if (attributeName === "description") {
+        this.updateDescription($key, newValue);
+      }
+      else if (attributeName === "imageSources") {
+        this.updateImageSources($key, newValue);
+      }
+      else if (attributeName === "ingredients") {
+        this.updateIngredients($key, newValue);
+      }
+      else if (attributeName === "name") {
+        this.updateName($key, newValue);
+      }
+      else if (attributeName === "rating") {
+        this.updateRating($key, newValue);
+      }
+    }
+  }
+
+  private updateSteps($key: string, newSteps: Array<string>): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { steps: newSteps })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
+  }
+
+  private updateAuthor($key: string, newAuthor: string): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { author: newAuthor })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
+  }
+
+  private updateAvatar($key: string, newAvatar: string): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { avatar: newAvatar })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
+  }
+
+  private updateDescription($key: string, newDescription: string): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { description: newDescription })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
+  }
+
+  private updateImageSources($key: string, newImageSources: Array<string>): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { imageSources: newImageSources })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
+  }
+
+  private updateIngredients($key: string, newIngredients: Array<string>): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { ingredients: newIngredients })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
+  }
+
+  private updateName($key: string, newName: string): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { name: newName })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
+  }
+
+  private updateRating($key: string, newRating: number): void {
+    this.af.database.list(PUBLIC_RECIPES_URL)
+      .update($key, { rating: newRating })
+      .then((_) => console.log('200: OK'), (err) => this.errorReportService.send(err.message));
   }
 }
