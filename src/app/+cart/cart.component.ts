bade6dcdc2745b1e4051cf89d72e290bbccb0999
justinlnx <justinlnx@gmail.com>
2017-03-20 22:37:55 -0700

    40 Create Cart UI Page

--- src/app/+cart/cart.component.ts
+++ src/app/+cart/cart.component.ts
@@ -1,5 +1,35 @@
 import {Component} from '@angular/core';
 
-@Component({selector: 'cart', template: `<h1>Cart</h1>`})
+@Component({
+  selector: 'cart',
+  template: `
+    <md-toolbar class="top-toolbar" color="primary">
+        <span>Cart</span>
+    </md-toolbar>
+    
+    <div class="page-content">
+        <md-tab-group>
+            <md-tab class="list-label" label="LIST">
+                <md-card>
+                    <md-card-title>
+                        <span class="md-headline">Recipe_1</span>
+                    </md-card-title>
+                    <md-card-subtitle>
+                        <span class="md-subhead">Author</span>
+                    </md-card-subtitle>
+                    <md-card-content>
+                        <md-card-actions>
+                            <button md-button>Action 1</button>
+                            <button md-button>Action 2</button>
+                        </md-card-actions>
+                    </md-card-content>
+                </md-card>    
+            </md-tab>
+            <md-tab class="location-label" label="LOCATION"></md-tab>
+        </md-tab-group>
+    </div>
+    `,
+  styleUrls: ['./cart.component.scss']
+})
 export class CartComponent {
 }
