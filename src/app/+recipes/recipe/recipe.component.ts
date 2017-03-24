d9773cb229485ecd7f68eee713c502af07db4346
Tianpei SHEN <Tianpei SHEN>
2017-03-23 23:10:59 -0700

    45cleanUP

--- src/app/+recipes/recipe/recipe.component.ts
+++ src/app/+recipes/recipe/recipe.component.ts
@@ -12,94 +12,96 @@ import {Rating} from './rating.component';
 @Component({
   selector: 'recipe',
   template: `
-  <md-toolbar class="top-toolbar" color="primary">
-    <span class = "recipeName">
-      <button md-button class="back-button" (click)="onNavigatingBack()">
-        <md-icon>arrow_back</md-icon>
-      </button>
-      {{recipe?.name}}
-    </span>
-    <md-icon (click)="likeRecipe(recipe); $event.stopPropagation()">
-      <span md-icon [class.fav-button]="recipe?.rating == '1'">favorite</span>
-    </md-icon>
-  </md-toolbar>
+    <md-toolbar class="top-toolbar" color="primary">
+      <span class = "recipeName">
+        <button md-button class="back-button" (click)="onNavigatingBack()">
+          <md-icon>
+            arrow_back
+          </md-icon>
+        </button>
+        {{recipe?.name}}
+      </span>
+      <md-icon (click)="likeRecipe(recipe); $event.stopPropagation()">
+        <span md-icon [class.fav-button]="recipe?.rating == '1'">favorite</span>
+      </md-icon>
+    </md-toolbar>
 
-  <div class="page-content">
+    <div class="page-content">
 
-    <md-card>
-      <md-card-title>AUTHOR</md-card-title>
-      <md-card-content>  
-      <md-list>
-      <md-list-item class="md-2-line">
-        <img md-card-avatar class = "avatar" [src]="recipe.avatar">
-         <div class="mat-list-text">
+      <md-card>
+        <md-card-title>AUTHOR</md-card-title>
+        <md-card-content>  
+          <md-list>
+          <md-list-item class="md-2-line">
+            <img md-card-avatar class = "avatar" [src]="recipe.avatar">
+            <div class="mat-list-text">
             <p md-line class = "name"> Shiba Inu </p>
-          </div>
-          <md-icon>message</md-icon>
-      </md-list-item>
-      </md-list>
-      </md-card-content>
-    </md-card>
+            </div>
+            <md-icon>
+              message
+            </md-icon>
+          </md-list-item>
+          </md-list>
+        </md-card-content>
+      </md-card>
 
-    <md-card>
+      <md-card>
         <md-card-title>DESCRIPTION</md-card-title>
         <md-card-content>
-            <p class = "description">{{recipe?.description}}</p>
+          <p class = "description">{{recipe?.description}}</p>
         </md-card-content>
-    </md-card>
+      </md-card>
 
-    <md-card>
-      <md-card-title>INGREDIENTS</md-card-title>
-      <md-card-content>
-        <md-list *ngFor="let ingredient of recipe?.ingredients; let i = index">
-            <p>{{ingredient}}</p>
-        </md-list>
-      </md-card-content>
-    </md-card>
+      <md-card>
+        <md-card-title>INGREDIENTS</md-card-title>
+        <md-card-content>
+          <md-list *ngFor="let ingredient of recipe?.ingredients">
+          <p>{{ingredient}}</p>
+          </md-list>
+        </md-card-content>
+      </md-card>
 
-    <md-card>
-      <md-card-title>STEPS</md-card-title>
-      <md-card-content>
-        <md-list *ngFor="let step of recipe?.steps; let i = index">
+      <md-card>
+        <md-card-title>STEPS</md-card-title>
+        <md-card-content>
+          <md-list *ngFor="let step of recipe?.steps; let i = index">
             <p>{{i+1}} {{step.content}}</p>
-            <img [src]="step.imageSource" style="width: 100%;max-height: 100%"> 
-                    alt="recipe image" style="width: 100%;max-height: 100%">
-        </md-list>
-      </md-card-content>
-    </md-card>
+            <img [src]="step.imageSource" style="width: 100%; max-height: 100%"> 
+            alt="recipe image" style="width: 100%;max-height: 100%">
+          </md-list>
+        </md-card-content>
+      </md-card>
 
-    <md-card>
-    <md-card-title>COMMENTS</md-card-title>
-    <md-card-content>
-      <div  *ngFor="let comment of recipe?.comments; let i = index">
+      <md-card>
+        <md-card-title>COMMENTS</md-card-title>
+        <md-card-content>
+          <div *ngFor="let comment of recipe?.comments">
           <md-card-content>
             <md-list>
               <md-list-item class="md-2-line">
                 <img md-card-avatar class = "avatar" [src]="recipe.avatar">
                 <div class="mat-list-text">
-                <p md-line class = "name"> Shiba Inu </p>
+                  <p md-line class = "name"> Shiba Inu </p>
                 </div>
               </md-list-item>
               <p>{{comment.content}}</p>
             </md-list>
-            <md-divider></md-divider>
+          <md-divider></md-divider>
           </md-card-content>
-      </div>
-
-
-      <md-card-content style="margin-bottom: 20px">
-        <div>
-            <textarea cols="40" rows="5"></textarea>
           </div>
-        <md-card-actions>
-          <button md-button>ADD COMMENT</button>
-          </md-card-actions>
-      </md-card-content>
 
-    </md-card-content>
-    </md-card>
+          <md-card-content style="margin-bottom: 20px">
+            <div>
+              <textarea cols="40" rows="5"></textarea>
+            </div>
+            <md-card-actions>
+              <button md-button>ADD COMMENT</button>
+            </md-card-actions>
+          </md-card-content>
+        </md-card-content>
+      </md-card>
 
-  </div>
+    </div>
   `,
   styleUrls: ['./recipe.component.scss'],
 })
@@ -133,15 +135,6 @@ export class RecipeComponent implements OnInit, OnDestroy {
   }
 
   public likeRecipe(recipe: Recipe) {
-    console.log(recipe.rating);
-    if (recipe.rating === 0) {
-      recipe.rating = 1;
-    } else {
-      recipe.rating = 0;
-    }
-  }
-
-    return this.domSanitizer.bypassSecurityTrustResourceUrl(imageSource);
-    }
+    this.apiService.toggleLike(recipe);
   }
 }
