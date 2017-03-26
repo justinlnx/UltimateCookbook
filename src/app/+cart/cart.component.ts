c3c31e2f07b2de83df5aa2e836b962c4c5605d40
xta13 <xta13@sfu.ca>
2017-03-26 08:37:56 +0800

    map showed

--- src/app/+cart/cart.component.ts
+++ src/app/+cart/cart.component.ts
@@ -36,11 +36,20 @@ import {Component} from '@angular/core';
                     </md-card-content>
                 </md-card>    
             </md-tab>
-            <md-tab class="location-label" label="LOCATION"></md-tab>
+            <md-tab class="location-label" label="LOCATION">
+                <h1>{{title}}</h1>
+                <sebm-google-map [latitude]="lat" [longitude]="lng">
+                    <sebm-google-map-marker [latitude]="lat" [longitude]="lng">
+                    </sebm-google-map-marker>
+                </sebm-google-map>
+            </md-tab>
         </md-tab-group>
     </div>
     `,
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  title: string = 'Google Map to find stores location';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 }
--- src/app/+cart/cart.module.ts
+++ src/app/+cart/cart.module.ts
@@ -1,11 +1,20 @@
 import {NgModule} from '@angular/core';
+
 import {RouterModule} from '@angular/router';
+import {AgmCoreModule} from 'angular2-google-maps/core';
 
 import {SharedModule} from '../shared';
 
 import {CartComponent} from './cart.component';
 import {routes} from './cart.routes';
 
-@NgModule({imports: [SharedModule, RouterModule.forChild(routes)], declarations: [CartComponent]})
+@NgModule({
+  imports: [
+    SharedModule, RouterModule.forChild(routes),
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyDrxvSMaiyZkfUZFZMDiRg_alqhYaOOIBk'})
+  ],
+  providers: [],
+  declarations: [CartComponent]
+})
 export class CartModule {
 }
