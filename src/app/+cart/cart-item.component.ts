c0de6770dffc3e1cdbfa7195da9e38c3261e97bb
wyc22 <wyc22@sfu.ca>
2017-03-27 17:05:00 -0700

    set each cart entry a height & expand/contrast cart entry on entry click #57

--- src/app/+cart/cart-item.component.ts
+++ src/app/+cart/cart-item.component.ts
@@ -5,7 +5,7 @@ import {ApiService, CartEntry, Ingredient, Recipe} from '../api';
 @Component({
   selector: 'cart-item',
   template: `
-  <md-card *ngIf="contentsExist()" class="mat-elevation-z8">
+  <md-card *ngIf="contentsExist()" class="mat-elevation-z8" (click)="onToggleContract()">
       <md-card-title>
           <span class="md-headline">{{recipe.name}}</span>
       </md-card-title>
@@ -13,11 +13,14 @@ import {ApiService, CartEntry, Ingredient, Recipe} from '../api';
           <span class="md-subhead">{{author}}</span>
       </md-card-subtitle>
       <md-card-content>
-          <md-list>
-              <md-list-item *ngFor="let ingredient of cartEntry.ingredients">
-                  <md-checkbox [ngModel]="ingredient.bought" (ngModelChange)="updateCartEntry(ingredient, $event)">
-                    {{ingredient.content}}
-                  </md-checkbox>
+          <md-list class="cart-ingredient-list">
+              <md-list-item class="cart-ingredient-item" *ngFor="let ingredient of filteredIngredients">
+                  <md-checkbox [ngModel]="ingredient.bought" (ngModelChange)="updateCartEntry(ingredient, $event)" (click)="$event.stopPropagation()">
+
+                  </md-checkbox>{{ingredient.content}}
+              </md-list-item>
+              <md-list-item *ngIf="contracted">
+                ...
               </md-list-item>
           </md-list>
       </md-card-content>
@@ -29,16 +32,40 @@ export class CartItemComponent implements OnInit {
   @Input() public cartEntry: CartEntry;
   public recipe: Recipe;
   public author: string;
+  public contracted: boolean;
+  public filteredIngredients: Ingredient[] = [];
 
-  constructor(public apiService: ApiService) {}
+  constructor(public apiService: ApiService) {
+    this.contracted = true;
+  }
 
   public ngOnInit() {
     this.getRecipeOnce();
     this.getAuthorOnce();
+
+    this.populateFilteredIngredients();
   }
 
   public contentsExist(): boolean {
     return !!this.cartEntry && !!this.recipe;
+  }
+
+  public show2Items(ingredients: Ingredient[]): Ingredient[] {
+    return ingredients.filter((_, index) => {
+      return index < 2;
+    });
+  }
+
+  public onToggleContract(): void {
+    this.contracted = !this.contracted;
+    this.populateFilteredIngredients();
+  }
+
+  public populateFilteredIngredients() {
+    this.filteredIngredients =
+        this.contracted ? this.show2Items(this.cartEntry.ingredients) : this.cartEntry.ingredients;
+
+    console.log(this.filteredIngredients);
   }
 
   public getRecipeOnce(): void {
