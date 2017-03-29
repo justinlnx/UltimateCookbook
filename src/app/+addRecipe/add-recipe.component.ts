dd3edce92bd3f83fd5907d456fc8a13b2fcc6cff
wyc22 <wyc22@sfu.ca>
2017-03-28 18:56:18 -0700

    allow adding steps and ingredients one by one #31

--- src/app/+addRecipe/add-recipe.component.ts
+++ src/app/+addRecipe/add-recipe.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AngularFire, FirebaseAuthState} from 'angularfire2';
 import {Subscription} from 'rxjs/Subscription';
@@ -11,24 +11,37 @@ import {ErrorReportService} from '../error-report';
   selector: 'add-recipe',
   template: `
   <md-toolbar class="top-toolbar" color="primary">
-    <span>Add new recipe</span>
+      <span>Add new recipe</span>
   </md-toolbar>
 
   <div class="page-content">
     <login-warning *ngIf="!userLoggedIn"></login-warning>
     <div class="add-field" *ngIf="userLoggedIn">
-      <form [formGroup]="addRecipeForm" novalidate>
-        <md-input-container>
-          <input mdInput placeholder="Name" type="text" formControlName="name">
+      <form class="recipeForm" [formGroup]="addRecipeForm" novalidate>
+        <md-input-container md-no-float>
+            <input mdInput placeholder="Title" type="text" formControlName="recipeName">
         </md-input-container>
-        <md-input-container>
-          <input mdInput placeholder="Description" type="text" formControlName="description">
+
+        <md-input-container md-no-float class="md-block">
+          <input mdInput placeholder="Description" type="text" formControlName="recipeDescription">
         </md-input-container>
-        <div>
-          <button md-raised-button
-                  [disabled]="!validAddRecipeForm" type="button"
-                  (click)="onAddRecipe()">Add</button>
+
+        <div formArrayName="stepDesc">
+        <md-input-container md-no-float *ngFor="let step of stepsArray.controls; let i = index" [formGroupName]="i">
+          <button md-icon-button class="addPhoto" (click)="uploadPhoto()">
+            <md-icon>add_a_photo</md-icon>
+          </button>
+          <textarea mdInput placeholder="Step {{i+1}}" type="text" formControlName="stepDescription"></textarea>
+        </md-input-container>
         </div>
+        <button md-raised-button (click)="addStep()">+ Step</button>
+
+        <div formArrayName="ingredientsList">
+        <md-input-container md-no-float *ngFor="let ingredient of ingredientsArray.controls; let i = index" [formGroupName]="i">
+          <input mdInput placeholder="Ingredient {{i+1}}" type="text" formControlName="ingredientDescription">
+        </md-input-container>
+        </div>
+        <button md-raised-button (click)="addIngredient()">+ Ingredient</button>
       </form>
     </div>
   </div>
@@ -67,13 +80,29 @@ export class AddRecipeComponent implements OnInit, OnDestroy {
     this.logInSubscription.unsubscribe();
   }
 
+  public addStep() {
+    this.stepsArray.push(this.initStep());
+  }
+
+  public addIngredient() {
+    this.ingredientsArray.push(this.initIngredient());
+  }
+
+  public uploadPhoto() {
+    console.log('add photo TODO');
+  }
+
   public onAddRecipe() {
     let avatar = '';
-    let name = this.addRecipeForm.value.name;
-    let description = this.addRecipeForm.value.description;
+    let name = this.addRecipeForm.value.recipeName;
+    let description = this.addRecipeForm.value.recipeDescription;
     let authorId = this.authState.uid;
-    let steps = [];
-    let ingredients = [];
+    let steps = this.stepsArray.controls.map((control) => {
+      return {content: control.value.stepDescription, imageSource: ''};
+    });
+    let ingredients = this.ingredientsArray.controls.map((control) => {
+      return control.value.ingredientDescription;
+    });
     let comments = [];
     let likedUsers = [];
     let newRecipe: PushRecipeSchema;
@@ -83,9 +112,37 @@ export class AddRecipeComponent implements OnInit, OnDestroy {
     this.nagivateToRecipesPage();
   }
 
+  get stepsArray(): FormArray {
+    return this.addRecipeForm.get('stepDesc') as FormArray;
+  }
+
+  get ingredientsArray(): FormArray {
+    return this.addRecipeForm.get('ingredientsList') as FormArray;
+  }
+
   private createAddRecipeForm() {
-    this.addRecipeForm = this.fb.group(
-        {name: ['', [Validators.required]], description: ['', [Validators.required]]});
+    this.addRecipeForm = this.fb.group({
+      recipeName: ['', [Validators.required]],
+      recipeDescription: ['', [Validators.required]],
+      stepDesc: this.fb.array([
+        this.initStep(),
+      ]),
+      ingredientsList: this.fb.array([
+        this.initIngredient(),
+      ])
+    });
+  }
+
+  private initStep() {
+    return this.fb.group({
+      stepDescription: ['', [Validators.required]],
+    });
+  }
+
+  private initIngredient() {
+    return this.fb.group({
+      ingredientDescription: ['', [Validators.required]],
+    });
   }
 
   private nagivateToRecipesPage() {
