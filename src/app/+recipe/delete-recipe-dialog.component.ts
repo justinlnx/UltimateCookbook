571ac1de78fbcf970fb66000fe5a0a82ee09ccd1
justinlnx <justinlnx@gmail.com>
2017-04-03 23:54:32 -0700

    71 Fix styling

--- src/app/+recipe/delete-recipe-dialog.component.ts
+++ src/app/+recipe/delete-recipe-dialog.component.ts
@@ -4,7 +4,9 @@ import {MdDialogRef} from '@angular/material';
 @Component({
   selector: 'delete-recipe-dialog',
   template: `
-    <h1 md-dialog-title>Delete Recipe</h1>
+    <div>
+      <h1 md-dialog-title>Delete Recipe</h1>
+    </div>
     <div md-dialog-content>Are you sure to delete this recipe?</div>
     <div md-dialog-actions>
         <button md-button (click)="dialogRef.close('Yes')">Yes</button>
